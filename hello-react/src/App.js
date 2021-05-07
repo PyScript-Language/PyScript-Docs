aba0a4978099ce2b209dbc4907f3014924032589
Glitch <>
2021-05-07 15:38:37 +0000

    Update Docs

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -9,14 +9,67 @@ function App() {
         <br>
         </br>
         <HomepageImage />
+        <h2>
+          The PyScript Docs
+        </h2>
         <h4>
           PyScript: A Python and Js language combined
         </h4>
         <br>
         </br>
-        <h6 align="right">
+        <br>
+        </br>
+        <h3>
+          Contents:
+        </h3>
+        <ol>
+          <li><a href="#intro">Introduction</a></li>
+          <li><a href="#syntax">Syntax</a></li>
+          <li><a href="#print">Printing</a></li>
+          <li><a href="#input">Inputs</a></li>
+          <li><a href="#var">Variables</a></li>
+
+
+          <li><a href="#examples">Examples</a></li>
+
+
+          <li><a href="#react">Learn Reactjs</a></li>
+        </ol>
+        <br>
+        </br>
+        <br>
+        </br>
+        <br>
+        </br>
+
+
+        <h3 id="intro">
+          Introduction to PyScript
+        </h3>
+        <p>
+          <strong>PyScript</strong> is a language; <strong>Python</strong> combined with <strong>JavaScript</strong>. The <a href="#syntax">syntax</a> will be talked about later in this document, but let's talk about the introduction for now ;)
+          <br></br>
+          It will be an object-orientated-programming language combined with scripts. The reason for this is because, like I said in the beginning, <strong>Python</strong> + <strong>JavaScript</strong>. Basically, it will have <strong>Python</strong> syntax and <strong>JavaScript</strong> syntax mashed together into <strong>PyScript</strong> <em>(which will later be talked about in the syntax section)</em>. I hope you enjoy hacking with PyScript! 
+        </p>
+        <h5><em><strong>Note: PyScript is not fully released yet, so be prepared to code with it soon!</strong></em></h5>
+        <br>
+        </br>
+        <br>
+        </br>
+        <br>
+        </br>
+
+        <h3 id="syntax">
+          Syntax for PyScript
+        </h3>
+        <p>
+          
+        </p>
+
+        <h6>
           <a
             className="App-link"
+            id="react"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
